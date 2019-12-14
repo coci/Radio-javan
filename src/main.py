56352d5fdf0bf4fb5b96afe26a5ff1aa43a2c96d
soroush <soroush_safarii@yahoo.com>
2019-12-14 21:37:50 +0330

    fix some label issue

--- src/main.py
+++ src/main.py
@@ -20,23 +20,20 @@ def action(url):
 	call scraper
 	:param url: url song in www.radiojavan.com
 	"""
-	if url:
+	if url.text():
+		url = url.text()
 		match = re.match(r"(https://www.radiojavan.com/mp3s)",url)
 		if match: 
 			radio_javan = RadioJavan(url=url)
 			if radio_javan.scrap():
 				final_url = radio_javan.scrap()
-				# TODO: add to label
-				pass
+				result_lbl.setText(final_url)
 			else:
-				# TODO: add to label
-				pass
+				result_lbl.setText("you must enter mp3 link")
 		else:
-			# TODO: add to label
-			pass
+			result_lbl.setText("please enter correct url")
 	else:
-		# TODO: add to label
-		pass
+		result_lbl.setText("please enter url !!!")
 
 # Create an instance of QApplication
 app = QApplication(sys.argv)
@@ -48,7 +45,7 @@ window.setGeometry(500, 300, 500, 300) # set size window
 
 # label that show what user must do
 main_lbl = QLabel('Please enter url :', parent=window)
-main_lbl.move(100, 50)
+main_lbl.move(140, 50)
 
 # input box
 url_input = QLineEdit(parent=window)
@@ -58,12 +55,15 @@ url_input.move(100, 80)
 
 # button to start scrap url
 btn = QPushButton('find :)',parent=window)
-btn.clicked.connect(partial(action,url_input.text())) # when button press call action()
+btn.clicked.connect(partial(action,url_input)) # when button press call action()
 btn.move(210, 140)
 
 # show result to this label
 result_lbl = QLabel('',parent=window)
-result_lbl.move(100, 170)
+result_lbl.setFixedWidth(400)
+
+result_lbl.move(50, 190)
+
 
 window.show() # show window
 sys.exit(app.exec_()) # keep alive app in mainloop
