9ef88d3a8030052bd845c2fee485166bdc26537d
soroush <soroush_safarii@yahoo.com>
2019-12-14 21:12:19 +0330

    bring match url into main.py

--- src/radio.py
+++ src/radio.py
@@ -1,5 +1,4 @@
 import requests
-import re
 import json
 
 class RadioJavan:
@@ -7,14 +6,14 @@ class RadioJavan:
 		self.url = url
 
 	def scrap(self):
-		match = re.match(r"(https://www.radiojavan.com/mp3s)",self.url)
-		if match:  
-			media_content = re.split(r'/',self.url)[4]
-			id_file = re.split(r'/',self.url)[5]
-			file_name = re.split(r'([?]([^\n]+))',id_file)[0]
+		media_content = re.split(r'/',self.url)[4]
+		id_file = re.split(r'/',self.url)[5]
+		file_name = re.split(r'([?]([^\n]+))',id_file)[0]
 
-			session = requests.Session()
-			if media_content == "mp3":
-				host_url = session.get("https://www.radiojavan.com/mp3s/mp3_host/?id=%s" %(file_name))
-				final_url = str(json.loads(host_url.text)['host'])+"/media/mp3/"+file_name+".mp3"
-				return final_url
+		session = requests.Session()
+		if media_content == "mp3":
+			host_url = session.get("https://www.radiojavan.com/mp3s/mp3_host/?id=%s" %(file_name))
+			final_url = str(json.loads(host_url.text)['host'])+"/media/mp3/"+file_name+".mp3"
+			return final_url
+		else:
+			return None
