25bb290b7b512fa90ade53f2a59ebdd820c98da6
soroush <soroush_safarii@yahoo.com>
2019-12-14 20:31:12 +0330

    radio javan scraper

---
+++ src/radio-javan.py
@@ -0,0 +1,20 @@
+import requests
+import re
+import json
+
+class RadioJavan:
+	def __init__(self,url):
+		self.url = url
+
+	def scrap(self):
+		match = re.match(r"(https://www.radiojavan.com/mp3s)",self.url)
+		if match:  
+			media_content = re.split(r'/',self.url)[4]
+			id_file = re.split(r'/',self.url)[5]
+			file_name = re.split(r'([?]([^\n]+))',id_file)[0]
+
+			session = requests.Session()
+			if media_content == "mp3":
+				host_url = session.get("https://www.radiojavan.com/mp3s/mp3_host/?id=%s" %(file_name))
+				final_url = str(json.loads(host_url.text)['host'])+"/media/mp3/"+file_name+".mp3"
+				return final_url
